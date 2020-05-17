079f7043f98454c6e194ba0c041232939adee9cb
Lauren Haaitsma <laurenhaaitsma@dutchcodersnetwork.nl>
2020-05-17 18:03:53 +0200

    Sending email thanks to https://www.callicoder.com/spring-boot-quartz-scheduler-email-scheduling-example/

--- src/main/java/com/lau/kittyShelter/service/NotifierService.java
+++ src/main/java/com/lau/kittyShelter/service/NotifierService.java
@@ -1,19 +1,20 @@
 package com.lau.kittyShelter.service;
 
+import com.lau.kittyShelter.service.email.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 public class NotifierService {
     @Autowired
     ShelterService shelterService;
 
+    @Autowired
+    MailService mailService;
+
     public void checkForNewKitty (){
         if(shelterService.isThereNewKittiesYet()){
-            System.out.println("sending Notification at "+ LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+            mailService.sendMail("l.haaitsma@gmail.com", "New Kitty!", "Hoi er is een nieuwe kitty yo!");
         }
     }
 }
---
+++ src/main/java/com/lau/kittyShelter/service/email/MailService.java
@@ -0,0 +1,43 @@
+package com.lau.kittyShelter.service.email;
+
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+@Getter
+@Service
+public class MailService {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+
+    public void sendMail(String toEmail, String subject, String body) {
+        try {
+            logger.info("Sending Email to {}", toEmail);
+            MimeMessage message = mailSender.createMimeMessage();
+
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+            messageHelper.setSubject(subject);
+            messageHelper.setText(body, true);
+            messageHelper.setFrom(mailProperties.getUsername());
+            messageHelper.setTo(toEmail);
+
+            mailSender.send(message);
+        } catch (MessagingException ex) {
+            logger.error("Failed to send email to {}", toEmail);
+        }
+    }
+}
